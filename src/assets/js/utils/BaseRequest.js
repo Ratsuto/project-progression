1ed65c362fe03a5ed54c0bbd5812fda4607fddd7
Ratsuto <sopharatt007@gmail.com>
2026-01-16 13:23:50 +0700

    first commit

---
+++ src/assets/js/utils/BaseRequest.js
@@ -0,0 +1,6 @@
+export const baseRequestClientSide = {
+    basicAuth: import.meta.env.VITE_BASIC_AUTH_USERNAME,
+    basicAuthPass: import.meta.env.VITE_BASIC_AUTH_PASSWORD,
+    username: '',
+    password: ''
+}
